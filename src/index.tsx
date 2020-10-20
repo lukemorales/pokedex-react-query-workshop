c43eaf8e0677dc8651893166fd754fee3170b5b7
Lucas Morales <lukemorales@live.com>
2020-10-20 00:10:52 -0300

    :fire: chore: remove redux once and for all :tada:

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
-import { Provider as ReduxProvider } from 'react-redux';
 
 import App from './App';
-import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <App />
-    </ReduxProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/store/modules/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { StoreState } from '../createStore';
-import pokemons from './pokemons/reducer';
-
-export default combineReducers<StoreState>({
-  pokemons,
-});
